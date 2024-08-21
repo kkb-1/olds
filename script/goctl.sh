1bf39b0f8e21ab66142941c223337bdfd3a3bbc9
kkbp <14321947+kkbp@user.noreply.gitee.com>
2024-08-22 00:50:15 +0800

    1.完善了readme 2.完善了脚本文件

--- script/goctl.sh
+++ script/goctl.sh
@@ -5,3 +5,16 @@ goctl rpc protoc *.proto --go_out=./ --go-grpc_out=./  --zrpc_out=./ --style=goZ
 
 #xcode生成pd go代码命令
 protoc --go_out=. ./pkg/xcode/types/status.proto
+
+# 配置拉取
+go mod init
+
+# 启动服务命令
+# 启动user同步mq服务
+go run ../apps/user-manage/mq/main.go
+# 启动user-manage rpc服务
+go run ../apps/user-manage/rpc/usermanage.go
+# 启动user rpc服务
+go run ../apps/user/rpc/user.go
+# 启动applet api服务
+go run ../apps/applet/user.go
